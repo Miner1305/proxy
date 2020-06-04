efda784a7460b75589d7cf90ca1f7360015855d7
Miner1305 <34150971+Miner1305@users.noreply.github.com>
2020-06-04 10:32:54 +0530

    Update Algorithm.cpp

--- src/base/crypto/Algorithm.cpp
+++ src/base/crypto/Algorithm.cpp
@@ -70,6 +70,7 @@ static AlgoName const algorithm_names[] = {
     { "cryptonight/rwz",           "cn/rwz",           Algorithm::CN_RWZ          },
     { "cryptonight/zls",           "cn/zls",           Algorithm::CN_ZLS          },
     { "cryptonight/double",        "cn/double",        Algorithm::CN_DOUBLE       },
+    { "cryptonight/conceal"        "cn/ccx",           Algorithm::CN_CCX          },
 #   ifdef XMRIG_ALGO_CN_GPU
     { "cryptonight/gpu",           "cn/gpu",           Algorithm::CN_GPU          },
     { "cryptonight_gpu",           nullptr,            Algorithm::CN_GPU          },
@@ -286,6 +287,7 @@ xmrig::Algorithm::Family xmrig::Algorithm::family(Id id)
     case CN_RWZ:
     case CN_ZLS:
     case CN_DOUBLE:
+    case CN_CCX:
 #   ifdef XMRIG_ALGO_CN_GPU
     case CN_GPU:
 #   endif
